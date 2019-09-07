f6d32889d5ee2951e97f792a208f85fd702c3022
jbedi07 <jayabedi1694@gmail.com>
2019-09-08 01:12:41 +0530

    initialize project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainningComponent } from './trainning/trainning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path : '', component:WelcomeComponent},
+ { path : 'signup', component:SignUpComponent},
+ { path : 'login', component:LoginComponent},
+ { path :'traning', component:TrainningComponent}
+
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+;
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule}  from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainningComponent } from './trainning/trainning.component';
+import { CurrentTrainingComponent } from './trainning/current-training/current-training.component';
+import { PastTrainingComponent } from './trainning/past-training/past-training.component';
+import { NewTrainingComponent } from './trainning/new-training/new-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    TrainningComponent,
+    CurrentTrainingComponent,
+    PastTrainingComponent,
+    NewTrainingComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
